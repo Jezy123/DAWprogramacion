2222b5cb984aca41dee2084a8d8baaea924871f1
Jezy123 <juanmapg321@gmail.com>
2021-09-29 09:34:59 +0200

    ejercicio4 terminado

---
+++ Ejemplo4.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class Ejemplo4{
+	public static void main (String argv[]){
+		float side;
+		float squareArea;
+
+		System.out.println("Enter the side:");
+	
+		Scanner inputValue;
+		inputValue = new Scanner(System.in);
+		side = inputValue.nextFloat();
+
+		squareArea = side * side;
+		System.out.println(squareArea);
+	}
+}
