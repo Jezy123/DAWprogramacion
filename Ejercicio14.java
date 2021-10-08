830688e62766fbc046d02f9c00b9f775c951330e
Jezy123 <juanmapg321@gmail.com>
2021-10-08 09:49:48 +0200

    Lo tenia bien optimizado desde el principio

--- Ejercicio14.java
+++ Ejercicio14.java
@@ -20,19 +20,19 @@ public class Ejercicio14 {
         if (nota>=0 && nota<3){
             System.out.println("Muy deficiente");
 
-        }else if ( nota<5){
+        }else if (nota>=3 && nota<5){
             System.out.println("Insuficiente");
 
-        }else if (nota<6){
+        }else if (nota>=5 && nota<6){
             System.out.println("Suficinte");
 
-        }else if (nota<7){
+        }else if (nota>=6 && nota<7){
             System.out.println("Bien");
 
-        }else if (nota<9){
+        }else if (nota>=7 && nota<9){
             System.out.println("Excelente");
 
-        }else if (nota<=10){
+        }else if (nota>=9 && nota<10){
             System.out.println("Sobresaliente");
         }
     
