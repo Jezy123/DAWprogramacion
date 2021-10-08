1cdda074e78f3d9317475bae8d8ee47a0c334e10
Jezy123 <juanmapg321@gmail.com>
2021-10-08 09:39:39 +0200

    Ejrcicio14 optimizado

--- Ejercicio14.java
+++ Ejercicio14.java
@@ -17,23 +17,23 @@ public class Ejercicio14 {
         inputValue.close();
 
         //Comparacion de varibales y casos dependiendo de lo que elija el Usuario
-        if (nota<3){
+        if (nota>=0 && nota<3){
             System.out.println("Muy deficiente");
 
-        }else if (nota>=3 && nota<5){
+        }else if ( nota<5){
             System.out.println("Insuficiente");
 
-        }else if (nota>=5 && nota<6){
+        }else if (nota<6){
             System.out.println("Suficinte");
 
-        }else if (nota>=6 && nota<7){
-            System.out.println("Introduce un numero positivo o negativo");
+        }else if (nota<7){
+            System.out.println("Bien");
 
-        }else if (nota>=7 && nota<9){
-            System.out.println("Introduce un numero positivo o negativo");
+        }else if (nota<9){
+            System.out.println("Excelente");
 
-        }else if (nota>=9 && nota<=10){
-            System.out.println("Introduce un numero positivo o negativo");
+        }else if (nota<=10){
+            System.out.println("Sobresaliente");
         }
     
     }
