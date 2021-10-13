0d452bc700fb58448d57e0706ba13013e1feb640
Jezy123 <juanmapg321@gmail.com>
2021-10-13 09:30:24 +0200

    control01 sin  optimizar

---
+++ Controles/Control01.java
@@ -0,0 +1,47 @@
+package Controles;
+import java.util.Scanner;
+public class Control01 {
+    public static void main(String[] args) {
+        
+        //Declaración de variables
+
+        int lados;
+        float longitud;
+
+
+        //Pido el numero de lados
+
+        System.out.println("Dime el número de lados del polígono:");
+
+        Scanner inputValue;
+        inputValue= new Scanner(System.in);
+        lados = inputValue.nextInt();
+        
+        //Compruebo si lados es 3 o 4 si nolo son el  programa acaba
+
+        if (lados == 3 || lados ==4){
+
+             //Pido la longitud
+
+            System.out.println("Dime la longitud del lado del polígono:");
+            longitud = inputValue.nextFloat();
+            inputValue.close();
+
+            //Dependiendo del numero de lado calculara el perimetro del triangulo o cuadrado 
+
+            switch(lados){
+                case 3:
+                System.out.println("El perimetro del triangulo es : "+ 3*longitud);
+                break;
+
+                case 4:
+                System.out.println("El perimetro del cuadrado es : "+ 4*longitud);
+                break;
+
+            }
+
+        }
+
+    }
+    
+}
