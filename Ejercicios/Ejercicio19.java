7f10ab164ae5edc1229d08045fb7b83f4add2db0
Jezy123 <juanmapg321@gmail.com>
2021-10-13 10:41:21 +0200

    Ejercicio19 sin optimizar

--- Ejercicios/Ejercicio19.java
+++ Ejercicios/Ejercicio19.java
@@ -3,9 +3,11 @@ import java.util.Scanner;
 public class Ejercicio19 {
     public static void main(String[] args) {
         
-        int horas;
-        int precioHora;
-        int GanadoSinImpuestos;
+        double horas;
+        double precioHora;
+        double GanadoSinImpuestos;
+        double GanadoNeto=0;
+        double Tasas=0;
 
 
         //Inicio de programa y obtencion de los numeros
@@ -13,16 +15,42 @@ public class Ejercicio19 {
         
         Scanner inputValue;
         inputValue = new Scanner(System.in);
-        horas =inputValue.nextInt();
+        horas =inputValue.nextDouble();
         System.out.println("Introduce elsalario por hora");
-        precioHora =inputValue.nextInt();
+        precioHora =inputValue.nextDouble();
         inputValue.close();
 
-        if (horas <35 ){
-            GanadoSinImpuestos = precioHora * 1,5 * (horas - 35 ) + precioHora * 35;
+        if (horas >35 ){
+            GanadoSinImpuestos = precioHora * 1.5 * (horas - 35 ) + precioHora * 35;
         }else{
             GanadoSinImpuestos = precioHora * 35;
         }
+
+        if (GanadoSinImpuestos<=500){
+
+            GanadoNeto=GanadoSinImpuestos;    
+            Tasas=0;
+            GanadoNeto= GanadoSinImpuestos-Tasas;
+
+        } else if (GanadoSinImpuestos<=900){
+               
+                Tasas= ((GanadoSinImpuestos-500)*0.25);
+
+        } else if (GanadoSinImpuestos>900){
+
+            Tasas=((GanadoSinImpuestos-900)*0.45) + (400*0.25);
+            GanadoNeto= GanadoSinImpuestos-Tasas;
+
+        }
+
+        System.out.println("El salario neto es de " + GanadoNeto);
+        System.out.println("Elsalario sin Impuestos es de "  + GanadoSinImpuestos);
+        System.out.println("Los impuestos son"+ Tasas);
+    }
+}
+
+
+
 
 
                
