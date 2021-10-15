d68540a4e88407cb85ba7eb13ae1872f2a142f02
Juanma Pérez <juanmapg321@gmail.com>
2021-10-15 02:53:25 +0200

    Ejercicio25 finalizado

---
+++ Ejercicios/Ejercicio25.java
@@ -0,0 +1,27 @@
+package Ejercicios;
+import java.util.Scanner;
+
+public class Ejercicio25 {
+    public static void main(String[] args) {
+
+        int numero;
+        int factorial;
+
+        System.out.println("Introduce un numero");
+
+        Scanner lector;
+        lector= new Scanner(System.in);
+        numero = lector.nextInt() ;
+        factorial = numero;
+
+
+        while (numero>1){
+            numero= numero -1;
+            factorial= factorial * numero;
+
+        }
+
+        System.out.println("Su factorial es "+ factorial);
+        lector.close();
+    }
+}
