96e73c57397757722432df9c81882a936c314cd0
Juanma Pérez <juanmapg321@gmail.com>
2021-10-15 02:39:13 +0200

    Ejercicio24 sin optimizar

---
+++ Ejercicios/Ejercicio24.java
@@ -0,0 +1,42 @@
+package Ejercicios;
+import java.util.Scanner;
+
+public class Ejercicio24 {
+    public static void main(String[] args) {
+
+        int nota;
+        int maxnota=0;
+        int media=0;
+        int finalizar=0;
+        int repeticiones= 0;
+        
+        Scanner inputValue;
+        inputValue = new Scanner(System.in);
+        System.out.println("Introduce -1 para finalizar el programa");
+
+        do {
+
+            System.out.println("Introduce una nota");
+    
+            nota = inputValue.nextInt();
+
+            if (nota == -1){
+                finalizar++;
+
+            }else{
+                media=media + nota;
+                repeticiones++;
+
+                if(nota==10){
+                    maxnota++;
+                }
+
+            }
+
+        }while (finalizar==0);
+
+        inputValue.close();
+    
+        System.out.println("Tu media es de " + media/repeticiones + " y has sacado un 10 " + maxnota + "veces");
+    }
+}
