c325f0f06c9a7d59b3542e09ea0a806a69af0eec
Jezy123 <juanmapg321@gmail.com>
2021-10-18 09:34:06 +0200

    Ejercicio34 hecho

--- Ejercicios/Ejercicio34.java
+++ Ejercicios/Ejercicio34.java
@@ -1,5 +1,22 @@
 package Ejercicios;
+import java.util.Scanner;
 
 public class Ejercicio34 {
+    public static void main(String[] args) {
+       
+        char[] ILetras={'X','Y','Z'};
+        char[] FLetras={'T','R','W','A','G','M','Y','F','P','D','X','B','N','J','Z','S','Q','V','H','L','C','K','E'};
     
+        int Nie;
+
+        Scanner lector;
+        System.out.println("Introducetu Nie sin la letra");
+        lector=new Scanner(System.in);
+        Nie= lector.nextInt();
+        lector.close();
+
+
+        System.out.println(FLetras[Nie%23]);
+        System.out.println(ILetras[Nie%3]);
+    }
 }
