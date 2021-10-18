27265c1060bf12855f7b058fa99fb3e16b7f77eb
Jezy123 <juanmapg321@gmail.com>
2021-10-18 09:25:11 +0200

    Ejercicio33 Acabado

---
+++ Ejercicios/Ejercicio33.java
@@ -0,0 +1,22 @@
+package Ejercicios;
+import java.util.Scanner;
+
+public class Ejercicio33 {
+    public static void main(String[] args) {
+       
+        final char[] Letras={'T','R','W','A','G','M','Y','F','P','D','X','B','N','J','Z','S','Q','V','H','L','C','K','E'};
+        int DNI;
+
+
+
+        Scanner lector;
+        System.out.println("Introducetu DNI sin la letra");
+        lector=new Scanner(System.in);
+        DNI= lector.nextInt();
+        lector.close();
+
+
+        System.out.println(Letras[DNI%23]);
+
+    }
+}
