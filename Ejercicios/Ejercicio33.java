3a79972bf1751352a2c8e290285057b5879a38df
Jezy123 <juanmapg321@gmail.com>
2021-10-18 09:28:01 +0200

    CAmbios

--- Ejercicios/Ejercicio33.java
+++ Ejercicios/Ejercicio33.java
@@ -4,7 +4,7 @@ import java.util.Scanner;
 public class Ejercicio33 {
     public static void main(String[] args) {
        
-        final char[] Letras={'T','R','W','A','G','M','Y','F','P','D','X','B','N','J','Z','S','Q','V','H','L','C','K','E'};
+        char[] Letras={'T','R','W','A','G','M','Y','F','P','D','X','B','N','J','Z','S','Q','V','H','L','C','K','E'};
         int DNI;
 
 
