538dcdf538d0fd3bf6f342488a13b86f109469a6
Jezy123 <juanmapg321@gmail.com>
2021-10-20 10:40:46 +0200

    Ejercicio35 a medias

---
+++ Ejercicios/Ejercicio35.java
@@ -0,0 +1,19 @@
+package Ejercicios;
+import java.util.Scanner;
+
+public class Ejercicio35 {
+    public static void main(String[] args) {
+        
+        int[] Cnota;
+        
+        Scanner lector;
+        lector = new Scanner(System.in);
+        System.out.println("Introduce -1 para finalizar el programa");
+
+        for (int i=0; i>finalizar; i++ ){
+            new anArray = new int[i]
+            anArray 
+        }
+
+
+       
