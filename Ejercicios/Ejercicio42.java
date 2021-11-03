d18ecfac34fe3adf007244ae7d598b3609fe095d
Jezy123 <juanmapg321@gmail.com>
2021-11-03 12:15:53 +0100

    Ejercicio42 hecho

---
+++ Ejercicios/Ejercicio42.java
@@ -0,0 +1,24 @@
+package Ejercicios;
+import java.util.Scanner;
+public class Ejercicio42{
+    public static void main(String[] args) {
+        
+        int n1;
+        int n2;
+        Scanner lector;
+        lector= new Scanner(System.in);
+
+        System.out.println("Introduce dos numeros");
+        n1=lector.nextInt();
+        n2=lector.nextInt();
+        int resultado=n1;
+
+        for (int i=1; i<n2;i++){
+            resultado+= n1;
+        }
+        System.out.println(resultado);
+        lector.close();
+
+    }
+}
+            
