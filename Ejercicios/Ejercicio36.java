e2cc1481267a0630b38f9dae4350efefcb1d1657
Jezy123 <juanmapg321@gmail.com>
2021-11-03 09:18:05 +0100

    Ejercicio36 realizado

--- Ejercicios/Ejercicio36.java
+++ Ejercicios/Ejercicio36.java
@@ -1,5 +1,34 @@
 package Ejercicios;
 
+import java.util.Scanner;
+
 public class Ejercicio36 {
-    
+    public static void main(String[] args) {
+        
+        int nmax;
+        int nIntroducido;
+        int[] Numeros;
+        Numeros = new int[10];
+
+        Scanner lector;
+        lector= new Scanner(System.in);
+
+        System.out.println("Introduce diez numeros");
+
+        for(int i=0; i<10; i++){
+            nIntroducido=lector.nextInt();
+            Numeros[i]=nIntroducido;
+
+        }
+        nmax= Numeros[0];
+        for(int i=0; i<10; i++){
+            if(nmax<Numeros[i]){
+                nmax=Numeros[i];
+            }
+        }
+        System.out.println("El numero mas grande es " + nmax);
+        lector.close();
+
+    }
+   
 }
