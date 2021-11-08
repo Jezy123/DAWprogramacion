e27117e3da22e464baed8b01e1ee747cd77ca5fa
Jezy123 <juanmapg321@gmail.com>
2021-11-08 09:14:24 +0100

    Ejercicio44 hecho creo quefunciona

--- Ejercicios/Ejercicio44.java
+++ Ejercicios/Ejercicio44.java
@@ -1,5 +1,27 @@
 package Ejercicios;
+import java.util.Scanner;
+public class Ejercicio44{
+    public static void main(String[] args) {
+        
+        String  binario;
+        int decimal=0;
+        int repeticiones=0;
+        
+        Scanner lector;
+        lector= new Scanner(System.in);
+        System.out.println("Introduceun numero binario");
+        binario=lector.nextLine();
+        lector.close();
 
-public class Ejercicio44 {
-    
+        for (int i=binario.length()-1;i>=0;i--){
+            char letra= binario.charAt(i);
+            if(letra=='1'){
+                decimal+= Math.pow(2, repeticiones);
+                
+            }
+            repeticiones++;
+
+        }
+        System.out.println(decimal);
+    }
 }
