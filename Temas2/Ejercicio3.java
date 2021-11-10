3cf5d897f25be9f03ab1f31b7157fa665253437f
Jezy123 <juanmapg321@gmail.com>
2021-11-10 09:56:50 +0100

    Ejercicio 3Tema 2

---
+++ Temas2/Ejercicio3.java
@@ -0,0 +1,8 @@
+package Temas2;
+public class Ejercicio3 {
+    public static void main(String[] args) {
+        System.out.println(Potencia.Elevado(2.1,2));
+        
+    }
+    
+}
---
+++ Temas2/Potencia.java
@@ -0,0 +1,11 @@
+package Temas2;
+
+public class Potencia {
+    public static double Elevado (double n1, int n2){
+        double elevado=Math.pow(n1, n2);
+        return elevado;
+        
+        
+    }
+    
+}
