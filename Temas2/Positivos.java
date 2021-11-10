dbbe4b4c9009aea95544ec3ba646088ee909d607
Jezy123 <juanmapg321@gmail.com>
2021-11-10 10:18:48 +0100

    Ejercicio4 t2 hecho

---
+++ Temas2/Positivos.java
@@ -0,0 +1,18 @@
+package Temas2;
+public class Positivos {
+    public static int positivos( int[]array) {
+
+        int Tnumeros=0;
+        
+        for(int i=0;i<=array.length-1;i++){
+            if(array[i]>0){
+                Tnumeros++;
+            }
+        }
+        return Tnumeros;
+        
+        
+    }
+
+    
+}
