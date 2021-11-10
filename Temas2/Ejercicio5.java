1437cc267c382ea30567d6e38a2907689fbb90f4
Jezy123 <juanmapg321@gmail.com>
2021-11-10 10:26:22 +0100

    Ejercicio5 hecho con su metodo

---
+++ Temas2/Ejercicio5.java
@@ -0,0 +1,9 @@
+package Temas2;
+
+public class Ejercicio5 {
+    public static void main(String[] args) {
+        int DNI=23526721;
+        System.out.println(LetraDNI.letraDNI(DNI));
+    }
+    
+}
