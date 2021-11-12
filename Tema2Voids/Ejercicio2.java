da65c67fb211c680edf3a9654ca71f2dc054948b
Jezy123 <juanmapg321@gmail.com>
2021-11-12 10:43:03 +0100

    Ejercicio2 voids a medias

---
+++ Tema2Voids/Ejercicio2.java
@@ -0,0 +1,18 @@
+package Tema2Voids;
+
+public class Ejercicio2 {
+    public static void descendente(int x , int y){
+        if(x>y){
+            System.out.println(x);
+            System.out.println(y);
+        }else{
+            System.out.println(x);
+            System.out.println(y);
+        }
+    }
+
+    public static void main(String[] args) {
+        int x
+    }
+    
+}
