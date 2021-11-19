8ce798f8cbf05fed7c21e8a87db5be299a0b478c
Jezy123 <juanmapg321@gmail.com>
2021-11-19 08:55:39 +0100

    Es Divisible metodos hecho

---
+++ Temas2/Ejercicio1.java
@@ -0,0 +1,8 @@
+package Temas2;
+
+public class Ejercicio1 {
+    public static void main(String[] args) {
+        System.out.println(EsDivisible.esDivisible(3, 8));
+    }
+    
+}
---
+++ Temas2/EsDivisible.java
@@ -0,0 +1,14 @@
+package Temas2;
+
+public class EsDivisible {
+    public static boolean esDivisible (int n, int m){
+        int residuo= n%m;
+        if(residuo==0){
+            return (true);
+        }else{
+            return (false);
+        }
+
+    }
+    
+}
