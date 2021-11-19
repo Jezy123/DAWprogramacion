4c9ae44fa7793b42f67509ce368b85789c21be47
Jezy123 <juanmapg321@gmail.com>
2021-11-19 09:05:12 +0100

    EsTriangulo hecho ejerccicio2

---
+++ Temas2/Ejercicio2.java
@@ -0,0 +1,8 @@
+package Temas2;
+
+public class Ejercicio2 {
+    public static void main(String[] args) {
+        System.out.println(EsTriangulo.esTriangulo(3, 6,11));
+    }
+    
+}
---
+++ Temas2/EsTriangulo.java
@@ -0,0 +1,12 @@
+package Temas2;
+
+public class EsTriangulo {
+    public static boolean esTriangulo (int n1,int n2, int n3){
+        if(n1>n2+n3||n2>n1+n3||n3>n1+n2){
+            return (false);
+        }else{
+            return (true);
+        }
+    }
+    
+}
