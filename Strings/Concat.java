27f77afae76abed4a72cdaa9d2db8e5e5a1a304c
Jezy123 <juanmapg321@gmail.com>
2021-12-03 08:31:19 +0100

    Contact junta las dos palabras

---
+++ Strings/Concat.java
@@ -0,0 +1,11 @@
+package Strings;
+
+public class Concat {
+    public static void main(String[] args) {
+        String Palabra1="Para";
+        String Palabra2="aguas";
+        System.out.println(Palabra1.concat(Palabra2));
+    
+    }
+    
+}
