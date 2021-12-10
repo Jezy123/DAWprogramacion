574038b16d9fddf7aaaa812e508baf7be60e7e11
Jezy123 <juanmapg321@gmail.com>
2021-12-10 10:29:21 +0100

    Last of Index funciona

---
+++ Strings/LastOfIndex.java
@@ -0,0 +1,11 @@
+package Strings;
+
+public class LastOfIndex {
+    public static void main(String[] args) {
+        String Texto1="Aqui hay otro texto más dominando más mejorado más buradamenmte dicho ";
+        String Texto2="más";
+        System.out.println(Texto1.lastIndexOf(Texto2));
+    }
+    
+}
+ 
