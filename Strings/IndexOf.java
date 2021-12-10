597faa8ea25ffe8f6b0ed40a5e1f5fcffc3cc0f1
Jezy123 <juanmapg321@gmail.com>
2021-12-10 09:16:39 +0100

    Index funciona

---
+++ Strings/IndexOf.java
@@ -0,0 +1,9 @@
+package Strings;
+
+public class IndexOf {
+    public static void main(String[] args) {
+        String Texto1="Esto es un texto de ejemplo perfectamente valido";
+        String Texto2="ejemplo perfectamente";
+        System.out.println("La cadena de texto2 aparece en el texto 1 en la casilla "+Texto1.indexOf(Texto2));
+    }
+}
