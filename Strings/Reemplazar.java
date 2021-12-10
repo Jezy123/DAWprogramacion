3c676713f507baf6bb8ec62fd0d8f91ee2d87240
Jezy123 <juanmapg321@gmail.com>
2021-12-10 09:16:14 +0100

    Reemplazar funciona correctamente

---
+++ Strings/Reemplazar.java
@@ -0,0 +1,9 @@
+package Strings;
+
+public class Reemplazar {
+    public static void main(String[] args) {
+        String Texto1="Aqui hay un texto de ejemplo";
+        System.out.println(Texto1.replace("o","e"));
+    }
+    
+}
