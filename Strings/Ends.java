9338b031e9372e7cd8dc112b901a53ca522f72f0
Jezy123 <juanmapg321@gmail.com>
2021-12-10 08:58:22 +0100

    Acabados en funciona

---
+++ Strings/Ends.java
@@ -0,0 +1,10 @@
+package Strings;
+
+public class Ends {
+    public static void main(String[] args) {
+        String Frase1 ="Esto es una frase";
+        String Terminacion="ase";
+        System.out.println(Frase1.endsWith(Terminacion));
+
+    }
+}
