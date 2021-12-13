94ef189626e854691f8917d39bd7dffd5aa917ed
Jezy123 <juanmapg321@gmail.com>
2021-12-13 09:44:40 +0100

    Longitud 3 hecho

---
+++ Strings/Longitud3.java
@@ -0,0 +1,32 @@
+package Strings;
+public class Longitud3 {
+    public static void main(String[] args) {
+      String Texto1=Utilidades.leerCadena("Introduce una frase");
+        int Lpalabra=0;
+        int Palabras4caracteres=0;
+        int PalabraGrande=0;
+        
+        Texto1= Texto1 + " ";
+        for (int i=0;i<Texto1.length();i++){
+            if(Texto1.charAt(i)!=' ' && Texto1.charAt(i)!=',' && Texto1.charAt(i)!='.' && Texto1.charAt(i)!='?' && Texto1.charAt(i)!='!'){
+                Lpalabra++;
+            }else{
+                if(Lpalabra==4){
+                    Palabras4caracteres++;
+                }
+                if(Lpalabra>4){
+                    PalabraGrande++;
+                }
+                Lpalabra=0;
+            }
+
+        }
+        if(PalabraGrande==0){
+            System.out.println("Ninguna palabra supera los 4 caracteres");
+        }if(PalabraGrande!=0){
+            System.out.println("Hay alguna palabra larga");
+        }
+        System.out.println("Hay un total de "+ Palabras4caracteres+ " palabras con 4 caracteres");
+    }   
+       
+}
