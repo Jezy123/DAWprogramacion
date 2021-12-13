4216bf8664a756b4c15ebd5fec0d95b84379202e
Jezy123 <juanmapg321@gmail.com>
2021-12-13 09:52:18 +0100

    Longitud con el numero que da el ususario

--- Strings/Longitud.java
+++ Strings/Longitud.java
@@ -5,20 +5,21 @@ public class Longitud {
       String Texto1=Utilidades.leerCadena("Introduce una frase");
         int Lpalabra=0;
         int Palabras4caracteres=0;
+        int Num=Utilidades.leerEntero("Introduza un numero");
         
         Texto1= Texto1 + " ";
         for (int i=0;i<Texto1.length();i++){
             if(Texto1.charAt(i)!=' ' && Texto1.charAt(i)!=',' && Texto1.charAt(i)!='.' && Texto1.charAt(i)!='?' && Texto1.charAt(i)!='!'){
                 Lpalabra++;
             }else{
-                if(Lpalabra==4){
+                if(Lpalabra==Num){
                     Palabras4caracteres++;
                 }
                 Lpalabra=0;
             }
 
         }
-        System.out.println("Hay un total de "+ Palabras4caracteres+ " palabras con 4 caracteres");
+        System.out.println("Hay un total de "+ Palabras4caracteres+ " palabras con "+Num +"caracteres");
     }
        
 }
