fce7e9206386c2256aa4ebb6ce9a02d7a88b91b8
Jezy123 <juanmapg321@gmail.com>
2021-12-13 09:49:49 +0100

    Longitud 2 con el numero que dice el ususario

--- Strings/Longitud2.java
+++ Strings/Longitud2.java
@@ -4,14 +4,15 @@ public class Longitud2 {
     public static void main(String[] args) {
       String Texto1=Utilidades.leerCadena("Introduce una frase");
         int Lpalabra=0;
+        int Num=Utilidades.leerEntero("Introduza un numero");
   
         Texto1= Texto1 + " ";
         for (int i=0;i<Texto1.length();i++){
             if(Texto1.charAt(i)!=' ' && Texto1.charAt(i)!=',' && Texto1.charAt(i)!='.' && Texto1.charAt(i)!='?' && Texto1.charAt(i)!='!'){
                 Lpalabra++;
             }else{
-                if(Lpalabra==4){
-                    System.out.println("Hay palabras con 4 caracteres");
+                if(Lpalabra==Num){
+                    System.out.println("Hay palabras con " +Num +" caracteres");
                     break;
                 }
                 Lpalabra=0;
