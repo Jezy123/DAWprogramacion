3dea4fcd21f3526e94c19efe608b61d45d801616
Jezy123 <juanmapg321@gmail.com>
2021-12-13 09:48:19 +0100

    Longitud 3 con el numero dice el usuario

--- Strings/Longitud3.java
+++ Strings/Longitud3.java
@@ -5,16 +5,17 @@ public class Longitud3 {
         int Lpalabra=0;
         int Palabras4caracteres=0;
         int PalabraGrande=0;
+        int Num=Utilidades.leerEntero("Introduza un numero");
         
         Texto1= Texto1 + " ";
         for (int i=0;i<Texto1.length();i++){
             if(Texto1.charAt(i)!=' ' && Texto1.charAt(i)!=',' && Texto1.charAt(i)!='.' && Texto1.charAt(i)!='?' && Texto1.charAt(i)!='!'){
                 Lpalabra++;
             }else{
-                if(Lpalabra==4){
+                if(Lpalabra==Num){
                     Palabras4caracteres++;
                 }
-                if(Lpalabra>4){
+                if(Lpalabra>Num){
                     PalabraGrande++;
                 }
                 Lpalabra=0;
@@ -22,11 +23,11 @@ public class Longitud3 {
 
         }
         if(PalabraGrande==0){
-            System.out.println("Ninguna palabra supera los 4 caracteres");
+            System.out.println("Ninguna palabra supera los "+Num +" carcteres");
         }if(PalabraGrande!=0){
             System.out.println("Hay alguna palabra larga");
         }
-        System.out.println("Hay un total de "+ Palabras4caracteres+ " palabras con 4 caracteres");
+        System.out.println("Hay un total de "+ Palabras4caracteres+ " palabras con "+Num+" caracteres");
     }   
        
 }
