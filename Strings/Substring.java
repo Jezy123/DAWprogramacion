435b8ec64423d0bfb2e68a2067f442e52918b385
Jezy123 <juanmapg321@gmail.com>
2021-12-13 08:28:02 +0100

    Substring funciona

---
+++ Strings/Substring.java
@@ -0,0 +1,8 @@
+package Strings;
+
+public class Substring {
+    public static void main(String[] args) {
+        String Cadena1="Esto es una cadena de texto que contiene palabras algo extrañas"; 
+        System.out.println(Cadena1.substring(5,9));
+    }
+}
