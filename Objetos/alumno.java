82e5d7651888ff39465ff772995341ad7b73b399
Jezy123 <juanmapg321@gmail.com>
2022-01-14 08:59:18 +0100

    alumnos bien

--- Objetos/alumno.java
+++ Objetos/alumno.java
@@ -6,19 +6,19 @@ package Objetos;
     que muestra el total de alumnos, otro metodo de clase que se llama imprimir informe alumnos al que se
     le pasa un array de alumnos y imprime el nombre y grupo de cada alumno*/
 
-class alumno{
+class Alumno{
     private int id;
     private String nombre;
     private String grupo;
     private static int numAlumnos;
 
-    public alumno(int i,String nom, String grup){
+    public Alumno(int i,String nom, String grup){
         id=i;
         nombre=nom;
         grupo=grup;
         numAlumn();
     }
-    public alumno(int i,String nom){
+    public Alumno(int i,String nom){
         id=i;
         nombre=nom;
         grupo="1CFS"; 
@@ -34,18 +34,19 @@ class alumno{
         numAlumnos++;
     }
     
-    static void imprimirInformeAlumnos(alumno arrayDeAlumnos[]){
+    /*static no es de un objeto concreto*/
+    static void imprimirInformeAlumnos(Alumno arrayDeAlumnos[]){
         for(int i=0;i<arrayDeAlumnos.length;i++){
             System.out.println(arrayDeAlumnos[i].nombre+" "+arrayDeAlumnos[i].grupo);
         }
     }
 
     public static void main(String[] args) {
-        alumno a1,a2,a3;
-        a1= new alumno(01,"Juanito","2CFS");
-        a2= new alumno(01,"Pepito","2CFS");
-        a3= new alumno(01,"ALejandrito");
-        alumno [] arrayDeAlumnos={a1,a2,a3};
+        Alumno a1,a2,a3;
+        a1= new Alumno(01,"Juanito","2CFS");
+        a2= new Alumno(01,"Pepito","2CFS");
+        a3= new Alumno(01,"ALejandrito");
+        Alumno [] arrayDeAlumnos={a1,a2,a3};
         a1.preguntarDuda();
         imprimirInformeAlumnos(arrayDeAlumnos);
     
