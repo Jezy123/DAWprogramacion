ea3831f8c1829b4cc50e62d4fa73110682322387
Jezy123 <juanmapg321@gmail.com>
2022-01-21 09:59:26 +0100

    TicketMachine

---
+++ Tema5/TestTicketMachine.java
@@ -0,0 +1,13 @@
+package Tema5;
+
+public class TestTicketMachine {
+    public static void main(String[] args) {
+        TicketMachine ticket1= new TicketMachine();
+            ticket1.setPrice(10);
+            ticket1.printTicket();
+             ticket1.getTotal();
+            ticket1.empty();
+
+    
+    }
+}
